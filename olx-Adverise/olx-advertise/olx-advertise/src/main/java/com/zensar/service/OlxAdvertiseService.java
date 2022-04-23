f08c38baea12ad2180ea5d4eb1bbb82100792ff3
Shravyaadepu <102963986+Shravyaadepu@users.noreply.github.com>
2022-04-23 09:48:27 +0530

    Add files via upload

---
+++ olx-Adverise/olx-advertise/olx-advertise/src/main/java/com/zensar/service/OlxAdvertiseService.java
@@ -0,0 +1,27 @@
+package com.zensar.service;
+
+
+import java.util.List;
+
+
+import com.zensar.olxadvertise.entity.Advertise;
+import com.zensar.olxadvertise.entity.AdvertiseDetails;
+
+public interface OlxAdvertiseService {
+	
+	 Advertise addAdvertise( Advertise advertise,String username,  String password);
+	 
+	 Advertise getAdvertise( long postId, String username, String password);
+     
+	 Advertise updateAdvertise( long id,  Advertise advertises, String username, String password);
+	 
+	 List<Advertise> getAllAdvertise( String username, String password);
+	 
+	 boolean deleteAdvertise( long id, String username, String password);
+	 
+	 Advertise searchAdvertisementsByCriteria( String category, String toDate,Long filtercriteria, String fromDate);
+	 
+	 List<Advertise> searchAdvertise();
+	 
+	 List<AdvertiseDetails> advertiseDetails( long id, String username, String password);
+}
